a222de843e0523df278cd4ee8368d701c8a50fea
Duds29 <eduardaelisacerqueira@gmail.com>
2023-08-16 20:48:14 -0300

    Remove unnecessary import in App.vue

--- front/app/src/App.vue
+++ front/app/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import NavBar from './components/NavBar.vue';
 import HomeView from './views/HomeView.vue'
 </script>
 
